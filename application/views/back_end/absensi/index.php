ceb909ff84554ac1dd1d451c203c203d275075e1
satria <satria@localhost>
2017-05-11 13:27:09 +0700

    add map alumni

--- application/views/back_end/absensi/index.php
+++ application/views/back_end/absensi/index.php
@@ -63,7 +63,7 @@ $next_list_number = isset($next_list_number) ? $next_list_number : 1;
                                                 </td>
                                                 <td>
                                                     <div class="btn-group btn-group-sm">
-                                                        <a class="btn btn-default" href="<?php echo base_url("back_end/" . $active_modul . "/detail") . "/" . $record->id; ?>">Approve</a>
+                                                        <a class="btn btn-default" href="<?php echo base_url("back_end/" . $active_modul . "/approve") . "/" . $record->id; ?>">Approved</a>
                                                         <a class="btn btn-default btn-hapus-row" href="javascript:void(0);" rel="<?php echo base_url("back_end/" . $active_modul . "/delete") . "/" . $record->id; ?>">REJECT</a>
                                                     </div>
                                                 </td>
